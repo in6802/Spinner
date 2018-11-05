937014137c6a7ecb732b6d11cb542ad9a0a1a66b
in6802 <in6802@gmail.com>
2018-11-05 15:25:28 +0900

    spinner list add

--- app/src/main/java/compinner/cji/edu/spinner/MainActivity.java
+++ app/src/main/java/compinner/cji/edu/spinner/MainActivity.java
@@ -2,6 +2,11 @@ package compinner.cji.edu.spinner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<String> list = new ArrayList<String>();
+        Spinner spinner = findViewById(R.id.spinner);
+
+        List<String> list = new ArrayList<>();
+        list.add("Apple");
+        list.add("Mango");
+        list.add("Banana");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
 
 
     }
