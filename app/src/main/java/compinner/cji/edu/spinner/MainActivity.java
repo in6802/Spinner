9f62f60171be4c07bbd9c53e35e2803a8fd367aa
in6802 <in6802@gmail.com>
2018-11-05 16:52:27 +0900

    .

--- app/src/main/java/compinner/cji/edu/spinner/MainActivity.java
+++ app/src/main/java/compinner/cji/edu/spinner/MainActivity.java
@@ -1,14 +1,21 @@
 package compinner.cji.edu.spinner;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-
+import android.widget.TextView;
+import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +25,50 @@ public class MainActivity extends AppCompatActivity {
         Spinner spinner = findViewById(R.id.spinner);
 
         List<String> list = new ArrayList<>();
+        list.add("Select!!");
         list.add("Apple");
         list.add("Mango");
         list.add("Banana");
 
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, list);
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(dataAdapter);
 
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+        String seleted = ((TextView)view).getText().toString();
+        String text = adapterView.getItemAtPosition(position).toString();
+
+        Toast.makeText(this, seleted + text, Toast.LENGTH_SHORT).show();
+
+        Intent intent;
+        // Intend
+        switch((int)id) {
+            case 1:
+                intent = new Intent(this, Spinner_sub1.class);
+                startActivityForResult(intent, Activity.RESULT_FIRST_USER);
+            break;
+            case 2:
+                intent = new Intent(this, Spinner_sub1.class);
+                startActivityForResult(intent, Activity.RESULT_FIRST_USER);
+                break;
+            case 3:
+                intent = new Intent(this, Spinner_sub1.class);
+                startActivityForResult(intent, Activity.RESULT_FIRST_USER);
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
---
+++ app/src/main/java/compinner/cji/edu/spinner/Spinner_sub1.java
@@ -0,0 +1,13 @@
+package compinner.cji.edu.spinner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Spinner_sub1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spinner_sub1);
+    }
+}
